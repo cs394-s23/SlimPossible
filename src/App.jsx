1b0338c2b480001a0d1b0d2eb804d184f04a4568
Erick Meikoki <erickmeikoki@Ericks-MacBook-Air.local>
2023-05-01 17:33:21 -0500

    log in bg black

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import "./App.css";
 import Login from "./Components/Authentication/Login";
 
 const App = () => {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+  return <Login />;
 };
 
 export default App;
