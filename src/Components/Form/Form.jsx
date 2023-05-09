e78d13c2d42c81994df454aa26fd163e8d8a5463
Erick Meikoki <erickmeikoki@Ericks-iMac.local>
2023-05-09 18:15:50 -0500

    search bar

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,60 +1,30 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
-const Form = () => {
-  const [foodName, setFoodName] = useState("");
-  const [ingredient1, seIngredient1] = useState("");
-  const [ingredient2, seIngredient2] = useState("");
-  const [ingredient3, seIngredient3] = useState("");
+function SearchForm() {
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const handleSubmit = () => {
-    console.log(foodName);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(searchQuery);
+  };
+
+  const handleSearchQueryChange = (event) => {
+    setSearchQuery(event.target.value);
   };
 
   return (
-    <div className="formPage">
-      <form className="enterFoodInfoform">
-        <div className="form-control">
-          <label>Meal Name: </label>
-          <input
-            type="text"
-            value={foodName}
-            onChange={(e) => setFoodName(e.target.value)}
-          />
-        </div>
+    <form onSubmit={handleSubmit}>
+      <label>Search: </label>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={handleSearchQueryChange}
+      />
 
-        <div className="form-control">
-          <label> Ingredient 1: </label>
-          <input
-            type="text"
-            value={ingredient1}
-            onChange={(e) => seIngredient1(e.target.value)}
-          />
-        </div>
-
-        <div className="form-control">
-          <label> Ingredient 2: </label>
-          <input
-            type="text"
-            value={ingredient2}
-            onChange={(e) => seIngredient2(e.target.value)}
-          />
-        </div>
-
-        <div className="form-control">
-          <label> Ingredient 3: </label>
-          <input
-            type="text"
-            value={ingredient3}
-            onChange={(e) => seIngredient3(e.target.value)}
-          />
-        </div>
-
-        <button type="submit"> Submit </button>
-      </form>
-    </div>
+      <button type="submit">Search</button>
+    </form>
   );
-};
+}
 
-export default Form;
+export default SearchForm;
