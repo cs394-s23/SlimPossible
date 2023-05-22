d6ffc370440527e98ad52522d6bf544341d7281d
billyin0702 <billyin0702@outlook.com>
2023-05-22 12:30:31 -0500

    Vite test file added

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -18,23 +18,14 @@ function SearchForm() {
   const [allMeals, setAllMeals] = useState([]);
 
   const fetchAllMeals = async () => {
-<<<<<<< HEAD
-=======
-
->>>>>>> 615604d (Vite test file added)
     const username = localStorage.getItem("email");
 
     const allMealsSnapshot = await getDocs(
       collection(db, "users", "user1", "all_meals")
     );
 
-<<<<<<< HEAD
-    const allMeals = allMealsSnapshot.docs.map((doc) => doc.data());
-    console.log("All user meals fetched!");
-=======
     const allMealsFromFirebase = await allMealsSnapshot.docs.map((doc) => doc.data());
     console.log("All user meals fetched!")
->>>>>>> 615604d (Vite test file added)
 
     setAllMeals(allMealsFromFirebase);
   };
